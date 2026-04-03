57e6595c4d8ba2a8e8865081566e205021620a64
Ken Sumang <ksumang77@gmail.com>
2026-04-04 03:10:51 +0800

    Update Javascript component injection function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
-async function loadComponent(id, file) {
-  const response = await fetch(file);
-  const data = await response.text();
-  document.getElementById(id).innerHTML = data;
+const base = window.location.hostname === "127.0.0.1"
+    ? 'http://127.0.0.1:5500'
+    : 'https://github.com/KenSumang/Kenaniah-Sumang-Portfolio.git';
+
+// async function loadComponent(id, file) {
+//     // console.log(`${base}${file}`);
+//     const response = await fetch(file);
+//     const data = await response.text();
+//     console.log(response);
+//     document.getElementById(id).innerHTML = data;
+// }
+
+async function loadComponent(selector, file) {
+    console.log(window.location.hostname);
+    console.log(`${base}${file}`);
+    let response = null;
+
+    if (base === "127.0.0.1") {  
+        response = await fetch(file);
+    } else {
+        response = await fetch(`${base}${file}`);
+    }
+    // const response = await fetch('${base}${file}');
+    console.log(response);
+    const html = await response.text();
+    document.getElementById(selector).innerHTML = html;
 }
 
 loadComponent("header", "/components/header.html");
 loadComponent("footer", "/components/footer.html");
 loadComponent("cta", "/components/cta.html");
 loadComponent("banner", "/components/banner.html");
-// loadComponent("contact", "/components/contact.html");
+// loadComponent("contact", "/components/contact.html");http://127.0.0.1:5500
