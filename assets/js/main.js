07a4bb942be70d0332cbf9d4298f3f912c4a9988
Ken Sumang <ksumang77@gmail.com>
2026-04-04 03:33:12 +0800

    Fix issue on javascript injection function. (1)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const base = window.location.hostname === "127.0.0.1"
     ? 'http://127.0.0.1:5500'
-    : 'https://github.com/KenSumang/Kenaniah-Sumang-Portfolio/blob/main';
+    : 'https://github.com/KenSumang/Kenaniah-Sumang-Portfolio';
 
 // async function loadComponent(id, file) {
 //     // console.log(`${base}${file}`);
@@ -12,13 +12,13 @@ const base = window.location.hostname === "127.0.0.1"
 
 async function loadComponent(selector, file) {
     console.log(window.location.hostname);
-    console.log(`${base}${file}`);
+    console.log(`${base}/components/${file}`);
     let response = null;
 
     if (base === "127.0.0.1") {  
         response = await fetch(file);
     } else {
-        response = await fetch(`${base}${file}`);
+        response = await fetch(`${base}/components/${file}`);
     }
     // const response = await fetch('${base}${file}');
     // console.log(response);
@@ -28,8 +28,8 @@ async function loadComponent(selector, file) {
     document.getElementById(selector).innerHTML = html;
 }
 
-loadComponent("header", "/components/header.html");
-loadComponent("footer", "/components/footer.html");
-loadComponent("cta", "/components/cta.html");
-loadComponent("banner", "/components/banner.html");
+loadComponent("header", "header.html");
+loadComponent("footer", "footer.html");
+loadComponent("cta", "cta.html");
+loadComponent("banner", "banner.html");
 // loadComponent("contact", "/components/contact.html");http://127.0.0.1:5500
